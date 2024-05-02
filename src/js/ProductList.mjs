2e6628ddaa5f1e4f501a1b45c35286b8473ee640
Heitor Cazado <heitorcazado@gmail.com>
2024-05-02 17:45:16 -0300

    w2 individual activity displaying cards

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -2,7 +2,7 @@ function productCardTemplate(product){
     return `
     <li class="product-card">
         <a href="product_pages/index.html?product=${product.Id}">
-        <img src="" alt="image of $${product.Name}">
+        <img src="${product.Image}" alt="image of ${product.Name}">
         <h3 class="card__brand">${product.Brand.Name}</h3>
         <h2 class="card__name">${product.NameWithoutBrand}</h2>
         <p class="product-card__price">$${product.FinalPrice}</p>
@@ -19,7 +19,8 @@ export default class ProductListing{
     }
 
     renderList(products){
-        this.listElement.innerHtml += products.map(productCardTemplate);
+        let html = products.map(productCardTemplate);
+        this.listElement.innerHTML = html;
     }
 
     async init(){
