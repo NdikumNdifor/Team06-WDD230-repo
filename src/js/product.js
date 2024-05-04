4afd14a217bbd9ed973b08fd0f1f8ccbc823ab18
NdikumNdifor <sebastienndifor@gmail.com>
2024-05-04 18:00:03 +0200

    Added changes to team activity

--- src/js/product.js
+++ src/js/product.js
@@ -1,25 +1,35 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage, getParams } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./productDetails.mjs";
 
+const productId = getParams('product');
 const dataSource = new ProductData("tents");
 
-function addProductToCart(product) {
-  let customerCart = getLocalStorage("so-cart");
-  if (customerCart == null) {
-    customerCart = [];
-    customerCart.push(product);
-  } else {
-    customerCart.push(product);
-  }
-  setLocalStorage("so-cart", customerCart);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+console.log(productId); // Just checking(can be deleted) if the id of a particular product is printed out.
+console.log(dataSource.findProductById(productId));// Just testing(can be deleted) out our findProductById method to see if we have a promiss to that product as well.
 
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+const product = new ProductDetails(productId, dataSource);
+product.init();
+
+
+
+// function addProductToCart(product) {
+//   let customerCart = getLocalStorage("so-cart");
+//   if (customerCart == null) {
+//     customerCart = [];
+//     customerCart.push(product);
+//   } else {
+//     customerCart.push(product);
+//   }
+//   setLocalStorage("so-cart", customerCart);
+// }
+// add to cart button event handler
+// async function addToCartHandler(e) {
+//   const product = await dataSource.findProductById(e.target.dataset.id);
+//   addProductToCart(product);
+// }
+
+// // add listener to Add to Cart button
+// document
+//   .getElementById("addToCart")
+//   .addEventListener("click", addToCartHandler);
