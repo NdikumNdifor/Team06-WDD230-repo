63da48949813638005d8375ed6845653d4356e19
sarihahope <sarihaslink@yahoo.com>
2024-05-04 00:00:51 -0700

    Tried to complete the week 2 activity - has errors.

--- src/js/cart.js
+++ src/js/cart.js
@@ -3,11 +3,11 @@ import { getLocalStorage } from "./utils.mjs";
 //get cart from local storage and verify if exists[has any item]
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
-  cartItems === null ? emptyCart()  : displayCart(cartItems);
+  cartItems === null ? emptyCart() : displayCart(cartItems);
 }
 
 //display an empty cart's message
-function emptyCart(){
+function emptyCart() {
   document.querySelector(".product-list").innerHTML = `
     <li>
       <h3>Sorry, you have no items in the cart!</h3>
@@ -15,10 +15,11 @@ function emptyCart(){
 }
 
 //map cart items into an appropriate displaying template
-function displayCart(cartItems){
+function displayCart(cartItems) {
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
 }
+
 
 function cartItemTemplate(item) {
   const newItem = `<li class="cart-card divider">
@@ -40,3 +41,4 @@ function cartItemTemplate(item) {
 }
 
 renderCartContents();
+
