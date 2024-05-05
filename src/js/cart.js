dd21b8d0fd3e02cba4f30fb8be4e786d9f3f7360
BrandonMHerrin <brandon.herrin@live.com>
2024-05-04 21:15:27 -0600

    complete: Implement Delete Button

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,13 +1,19 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+
+function getCartItems() {
+  return getLocalStorage("so-cart");
+}
 
 //get cart from local storage and verify if exists[has any item]
 function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
-  cartItems === null ? emptyCart()  : displayCart(cartItems);
+  const cartItems = getCartItems();
+  cartItems === null || cartItems.length === 0
+    ? emptyCart()
+    : displayCart(cartItems);
 }
 
 //display an empty cart's message
-function emptyCart(){
+function emptyCart() {
   document.querySelector(".product-list").innerHTML = `
     <li>
       <h3>Sorry, you have no items in the cart!</h3>
@@ -15,28 +21,48 @@ function emptyCart(){
 }
 
 //map cart items into an appropriate displaying template
-function displayCart(cartItems){
+function displayCart(cartItems) {
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
 }
 
+//
 function cartItemTemplate(item) {
-  const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
+  const newItem = `
+  <li class="cart-card divider">
+    <a href="#" class="cart-card__image">
+      <img
+        src="${item.Image}"
+        alt="${item.Name}"
+      />
+    </a>
+    <a href="#">
+      <h2 class="card__name">${item.Name}</h2>
+    </a>
+    <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+    <p class="cart-card__quantity"><span>qty: 1</span><button id="${item.Id}" class="delete-button">X</button></p>
+    <p class="cart-card__price">$${item.FinalPrice}</p>
+  </li>`;
 
   return newItem;
 }
 
+function removeItem(item) {
+  const cartItems = getCartItems();
+  const newItemsArray = cartItems.filter((cItem) => cItem.Id !== item);
+  setLocalStorage("so-cart", newItemsArray);
+}
+
+function deleteClickHandlder(event) {
+  const element = event.target;
+  if (element.classList.contains("delete-button")) {
+    removeItem(element.id);
+    renderCartContents();
+  }
+}
+
 renderCartContents();
+
+document
+  .querySelector(".product-list")
+  .addEventListener("click", deleteClickHandlder);
