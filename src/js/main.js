478abe98fc0f29065923f8c2467347da008054fa
NdikumNdifor <sebastienndifor@gmail.com>
2024-05-08 11:29:39 +0200

    Added customizable alerts to index.html

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import ProductData from "./ProductData.mjs";
 
 import ProductListing from "./ProductList.mjs"
+import Alert from "./alert.mjs";
 
 const main = async () => {
   const category = "tents";
@@ -12,6 +13,11 @@ const main = async () => {
   const topFourTentIds = ["880RR", "985RF", "985PR", "344YJ"];
   const topFourTents = tents.filter((product) => topFourTentIds.includes(product.Id))
   productList.renderList(topFourTents);
+
+
+  const dataSource1 = new ProductData("alerts")
+  const alerts = new Alert("alerts", dataSource1)
+  alerts.init();
 }
 
 main();
