4cdb3d909dd96b3f7829a63ce54db396b869194e
sarihahope <sarihaslink@yahoo.com>
2024-05-10 19:20:37 -0700

    Don't look, it broken

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,27 @@
-import ProductData from "./ProductData.mjs";
-import ProductListing from "./ProductList.mjs"
-import Alert from "./alert.mjs";
-import { loadHeaderFooter } from "./utils.mjs"
+// import ProductData from "./ProductData.mjs";
+// import ProductListing from "./ProductList.mjs"
+// import Alert from "./alert.mjs";
+// import { loadHeaderFooter } from "./utils.mjs"
 
-const main = async () => {
-  const category = "tents";
-  const dataSource = new ProductData(category);
-  const listElement = document.querySelector(".product-list")
-  const productList = new ProductListing(category, dataSource, listElement);
-  await productList.init();
-  const tents = productList.products;
-  const topFourTentIds = ["880RR", "985RF", "985PR", "344YJ"];
-  const topFourTents = tents.filter((product) => topFourTentIds.includes(product.Id))
-  productList.renderList(topFourTents);
+// const main = async () => {
+//   const category = "tents";
+//   const dataSource = new ProductData(category);
+//   const listElement = document.querySelector(".product-list")
+//   const productList = new ProductListing(category, dataSource, listElement);
+//   await productList.init();
+//   const tents = productList.products;
+//   const topFourTentIds = ["880RR", "985RF", "985PR", "344YJ"];
+//   const topFourTents = tents.filter((product) => topFourTentIds.includes(product.Id))
+//   productList.renderList(topFourTents);
 
 
-  const dataSource1 = new ProductData("alerts")
-  const alerts = new Alert("alerts", dataSource1)
-  alerts.init();
-  await loadHeaderFooter();
-}
+//   const dataSource1 = new ProductData("alerts")
+//   const alerts = new Alert("alerts", dataSource1)
+//   alerts.init();
+//   await loadHeaderFooter();
+// }
 
-main();
+// main();
+import { loadHeaderFooter } from "./utils.mjs";
+
+loadHeaderFooter();
