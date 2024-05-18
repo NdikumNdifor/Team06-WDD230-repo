54cb011bec8d458b0c335c3c3eb0ecfe05f8097b
Heitor Cazado <heitorcazado@gmail.com>
2024-05-18 17:46:38 -0300

    completed card product colors

--- src/js/product.js
+++ src/js/product.js
@@ -18,9 +18,17 @@ async function main() {
   document
     .getElementById("addToCart")
     .addEventListener("click", addToCartHandler);
+  
+    document.getElementById("color")
+    .addEventListener("change", updatePreviewColorHandler);
 }
 
 main();
+
+async function updatePreviewColorHandler(e){
+  const previewColorImages = document.querySelectorAll(".preview-color");
+  productDetail.updatePreviewImage(e, previewColorImages);
+}
 
 // add to cart button event handler
 async function addToCartHandler(e) {
